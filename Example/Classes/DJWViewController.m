0ce9625eebeb94d40eb9b34e23a3837bc2d4432b
Ian G <yo.ian.g@gmail.com>
2015-06-22 13:04:43 -0400

    Updated Example to use Rating Changed Callback

--- Example/Classes/DJWViewController.m
+++ Example/Classes/DJWViewController.m
@@ -25,6 +25,17 @@
     anotherStarRatingView.center = self.view.center;
     anotherStarRatingView.editable = YES;
     anotherStarRatingView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    
+    
+    UILabel *starRatingValue = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width / 2 - 100, self.view.bounds.size.height / 2 + 20, 200, 40) ];
+    [self.view addSubview:starRatingValue];
+    starRatingValue.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    starRatingValue.textColor = [UIColor whiteColor];
+    starRatingValue.textAlignment = NSTextAlignmentCenter;
+    
+    anotherStarRatingView.ratingChanged = ^(float newValue) {
+        starRatingValue.text = [NSString stringWithFormat:@"Value updated to %.1f", newValue];
+    };
 }
 
 @end
